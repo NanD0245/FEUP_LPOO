2c96609691abc088e562f0d71956f78a3a6748b1
up201905951 <up201905951@fe.up.pt>
2021-02-24 11:27:13 +0000

    hero class

--- hero/src/main/java/Game.java
+++ hero/src/main/java/Game.java
@@ -16,28 +16,25 @@ import static java.lang.System.*;
 
 public class Game {
     private Screen screen;
-    private int x = 10;
-    private int y = 10;
+    private Hero hero;
 
     Game() throws IOException {
         TerminalSize terminalSize = new TerminalSize(125, 40);
         DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory()
                 .setInitialTerminalSize(terminalSize);
         Terminal terminal = terminalFactory.createTerminal();
-        //int height = ((AWTTerminal)terminal).getFontConfiguration().getFontHeight();
-        //System.out.print(height);
-        //System.out.println("A message which is logged at QUIET level");
-        //Terminal terminal = new DefaultTerminalFactory().createTerminal();
-        screen = new TerminalScreen(terminal);
 
+        screen = new TerminalScreen(terminal);
         screen.setCursorPosition(null);   // we don't need a cursor
         screen.startScreen();             // screens must be started
         screen.doResizeIfNecessary();     // resize screen if necessary
+
+        hero = new Hero(10,10);
     }
 
     private void draw() throws IOException {
         screen.clear();
-        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
+        hero.draw(screen);
         screen.refresh();
     }
 
@@ -48,25 +45,29 @@ public class Game {
         while (check) {
             switch (key.getKeyType()) {
                 case ArrowUp:
-                    if (y > 0) {
+                    if (hero.getY() > 0) {
                         screen.clear();
-                        screen.setCharacter(x, --y, TextCharacter.fromCharacter('X')[0]);
+                        hero.moveUp();
+                        hero.draw(screen);
                         screen.refresh();
                     }
                     break;
                 case ArrowDown:
                     screen.clear();
-                    screen.setCharacter(x, ++y, TextCharacter.fromCharacter('X')[0]);
+                    hero.moveDown();
+                    hero.draw(screen);
                     screen.refresh();
                     break;
                 case ArrowLeft:
                     screen.clear();
-                    screen.setCharacter(--x, y, TextCharacter.fromCharacter('X')[0]);
+                    hero.moveLeft();
+                    hero.draw(screen);
                     screen.refresh();
                     break;
                 case ArrowRight:
                     screen.clear();
-                    screen.setCharacter(++x, y, TextCharacter.fromCharacter('X')[0]);
+                    hero.moveRight();
+                    hero.draw(screen);
                     screen.refresh();
                     break;
                 case Character:
@@ -78,18 +79,8 @@ public class Game {
                     check = false;
                     break;
             }
-            /*if (key.getKeyType() == KeyType.ArrowUp) {
-                screen.clear();
-                screen.setCharacter(x + 5, y + 5, TextCharacter.fromCharacter('X')[0]);
-                screen.refresh();
-            }
-            if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q') {
-                screen.setCharacter(x - 5, y - 5, TextCharacter.fromCharacter('Q')[0]);
-                screen.refresh();
-            }*/
             key = screen.readInput();
         }
-        //processKey(key);
     }
 
     private void processKey(KeyStroke key) {
---
+++ hero/src/main/java/Hero.java
@@ -0,0 +1,38 @@
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.Screen;
+
+public class Hero {
+    private int x;
+    private int y;
+
+    Hero(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void draw(Screen screen) {
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
+    }
+
+    public void moveUp() { y--; }
+    public void moveDown() { y++; }
+    public void moveLeft() { x--; }
+    public void moveRight() { x++; }
+
+}
